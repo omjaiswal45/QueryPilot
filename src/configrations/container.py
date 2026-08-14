cabdca3dff584c2a7bb69389509af89664371b68
omjaiswal45 <omjai11022000@gmail.com>
2026-08-14 23:46:12 +0530

    containerupdate

--- src/configrations/container.py
+++ src/configrations/container.py
@@ -8,3 +8,40 @@ TODO:
 - get_business_db_connector() -> returns infrastructures/db/business_db_connector.py
 - get_query_service() -> builds QueryService with its repository/connector deps injected
 """
+from pymongo import MongoClient
+
+from configurations.container import Container
+from configurations.google_auth_config import GoogleAuthConfig
+from configurations.mongo_config import MongoConfig
+from infrastructure.mongo_client import MongoClientProvider
+from configurations.llm_config import LlmConfig
+from configurations.logger import AppLogger
+logger = AppLogger.get_logger(__name__)
+
+class ConfigRegisterer:
+
+    @classmethod
+    def register_configs(cls, env):
+        try:
+            logger.info("Initializing Configs")
+            container = Container()
+
+            google_auth_cfg = GoogleAuthConfig(env)
+            container.register(GoogleAuthConfig, instance=google_auth_cfg)
+
+            mongo_config = MongoConfig(env)
+            container.register(MongoConfig, instance=mongo_config)
+            logger.info("Mongo configuration registered successfully")
+
+            mongo_client: MongoClient = MongoClientProvider.create_mongo_client()
+            container.register(MongoClient, instance=mongo_client)
+            logger.info("Mongo client registered successfully")
+
+            llm_config = LlmConfig(env)
+            container.register(LlmConfig, instance=llm_config)
+            logger.info("LLM configuration registered successfully")
+
+
+        except Exception as e:
+            logger.info(f"Error initiaizing configs: {e}")
+            raise
